92a1a4245dd0fbe649dfa091a73c984e3271f769
terry-man-com <terry.man.com@icloud.com>
2022-04-03 09:59:27 +0900

    ペットショップ１課題修正版

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
     <title>ペットショップアプリ</title>
 </head>
 <body>
